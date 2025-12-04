14c50326f8fa72f11fdd9dee02f5a38f2ab12865
HaixD <149478749+HaixD@users.noreply.github.com>
2025-12-03 18:49:06 -0800

    fix posterior calculation and popping

--- algorithms/belief_grid.py
+++ algorithms/belief_grid.py
@@ -1,6 +1,6 @@
+from math import log
 from random import randint
 from enum import IntEnum
-from heapq import heappush, heappop
 
 class Cell(IntEnum): # PLACEHOLDER
     EMPTY = 0
@@ -17,6 +17,33 @@ class Cell(IntEnum): # PLACEHOLDER
     PATH_SECOND = 11
     PATH_BOTH = 12
 
+def update_posterior(observations, prior, fp, fn):
+    true_positive = 1 - fp
+    true_negative = 1 - fn
+
+    T_likelihood = 1
+    not_T_likelihood = 1
+    for observation in observations:
+        if observation:
+            T_likelihood *= true_positive
+            not_T_likelihood *= fp
+        else:
+            T_likelihood *= fn
+            not_T_likelihood *= true_negative
+
+    T_posterior = prior * T_likelihood
+    not_T_posterior = (1 - prior)  * not_T_likelihood
+
+    return T_posterior / (T_posterior + not_T_posterior)
+
+res = update_posterior(
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    2 / 70,
+    0.1,
+    0.2
+)
+##print(res)
+
 class BeliefGrid:
     def __init__(self, grid, false_positive, false_negative):
         self.grid = grid
@@ -25,7 +52,6 @@ class BeliefGrid:
         
         self.observations = [[[] for _ in range(len(grid[0]))] for _ in range(len(grid))]
         self.beliefs = [[None] * len(grid[0]) for _ in range(len(grid))]
-        self.beliefs_heap = []
         self.popped = set()
 
         self.treasures = 1
@@ -43,12 +69,6 @@ class BeliefGrid:
                 if col is not None:
                     self.update_posterior(r, c)
 
-        old_heap = self.beliefs_heap
-        self.beliefs_heap = []
-        for _, position in old_heap:
-            r, c = position
-            heappush((-self.beliefs[r][c], position))
-
     def scan(self, r, c):
         if self.grid[r][c] == Cell.TREASURE:
             if randint(1, 100) / 100 <= self.false_negative:
@@ -65,7 +85,7 @@ class BeliefGrid:
 
     def update_posterior(self, r, c):
         true_positive = 1 - self.false_positive
-        false_negative = 1 - self.false_negative
+        true_negative = 1 - self.false_negative
 
         T_likelihood = 1
         not_T_likelihood = 1
@@ -74,27 +94,44 @@ class BeliefGrid:
                 T_likelihood *= true_positive
                 not_T_likelihood *= self.false_positive
             else:
-                T_likelihood *= false_negative
-                not_T_likelihood *= self.false_negative
+                T_likelihood *= self.false_negative
+                not_T_likelihood *= true_negative
 
         T_posterior = self.prior * T_likelihood
         not_T_posterior = (1 - self.prior)  * not_T_likelihood
 
         self.beliefs[r][c] = T_posterior / (T_posterior + not_T_posterior)
 
-        position = (r, c)
-        if position not in self.popped:
-            self.beliefs_heap.append((-self.beliefs[r][c], position))
+    def pop(self, *, error=True):
+        output = (-1, None) # (belief, position)
+        for r, row in enumerate(self.beliefs):
+            for c, belief in enumerate(row):
+                position = (r, c)
+                if belief is None or position in self.popped:
+                    continue
 
-    def pop(self):
-        _, position = heappop(self.beliefs_heap)
-        (r, c) = position
+                output = max(output, (belief, position), key=lambda v : v[0])
 
-        if position in self.popped:
-            return self.pop()
-        self.popped.add(position)
+        position = output[1]
 
-        return r, c
+        if position is None:
+            if error:
+                raise IndexError("pop from empty list")
+        else:
+            self.popped.add(position)
+
+        return position
+
+    def get_entropy(self):
+        entropy = 0
+
+        for row in self.beliefs:
+            for belief in row:
+                if belief is None:
+                    belief = self.prior
+                entropy += -belief * log(belief + 1e-12)
+
+        return entropy
 
 if __name__ == '__main__':
     grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -104,12 +141,16 @@ if __name__ == '__main__':
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    h, w = len(grid), len(grid[0])
+            
     belief_grid = BeliefGrid(grid, 0.1, 0.2)
-    for _ in range(100):
-        belief_grid.scan(randint(0, len(grid) - 1), randint(0, len(grid[0]) - 1))
-
-    while True:
+    print(belief_grid.get_entropy())
+    for _ in range(len(grid) * len(grid[0]) * 100):
+        belief_grid.scan(randint(0, h - 1), randint(0, w - 1))
+    print(belief_grid.get_entropy())
+    for _ in range(4):
         try:
-            print(belief_grid.pop())
-        except Exception:
+            r, c = belief_grid.pop()
+            print((r, c), grid[r][c] == 2)
+        except IndexError:
             break
